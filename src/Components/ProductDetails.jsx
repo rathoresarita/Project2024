18a3530e2d88c6897042bb0cdb1f1f970952ab0b
rathore <saritasinghrathore140@gmail.com>
2024-02-20 22:54:48 +0530

    added

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const ProductDetails = () => {
   return (
-    <div>ProductDetails</div>
+    <div className='mb-1'> clProductDetails
+    
+    <div className='text-xl xl:text-2xl'> {product.title}</div>
+   
+    <div>{product.brand}</div>
+    <div>{product.avgRating}</div>
+   <div>{product.attribute}</div>
+
+   <div>{product.badge}</div>
+    </div>
   )
 }
