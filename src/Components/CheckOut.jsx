b7c4df41576d3c2ecf6658fdc4bd77860cbae1f0
rathore <saritasinghrathore140@gmail.com>
2024-02-29 10:21:17 +0530

    added

--- src/Components/CheckOut.jsx
+++ src/Components/CheckOut.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const CheckOut = () => {
     return (
-        <div>
-            CheckOut
+        <div className='h-screen bg-amazoncone-background'>
+            <div  className='min-w-[1000px] mx-w-[1500px] m-auto pt-8'> 
+<div className='grid grid-cols-8 bg-white'>
+    {/* Products */}
+    <div className='col-span-6'>
+        <div className='text-2xl xl:text-3xl m-4'> Shopping Cart</div>
+
+
+    </div>
+    {/* Cart */}
+    <div className='col-span-2'></div>
+  
+
+</div>
+            </div>
+            
         </div>
     )
 }
