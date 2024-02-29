a50f5502c04863a58940de2eb49a8bc6e7aafe0b
rathore <saritasinghrathore140@gmail.com>
2024-02-29 15:31:14 +0530

    added

--- src/Components/CheckOut.jsx
+++ src/Components/CheckOut.jsx
@@ -1,23 +1,97 @@
-import React from 'react'
+import React, { useDebugValue } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import ProductDetails from './ProductDetails'
+import { GB_CURRENCY } from '../utils/Constants'
+
+import { removeFromCart } from '../Components/redux/cartSlice'
+
 
 const CheckOut = () => {
+    const products = useSelector((state) => state.cart.products)
+    console.log('products', products)
+   const dispatch=useDispatch()
+
+
+    const subtotal = useSelector((state) => state.cart.products.reduce((subtotal, product) => subtotal + (product.price * product.quantity), 0))
+    console.log('subtotal ', subtotal)
+
+    const itmesNumber = useSelector((state) => state.cart.productsNumber)
+    console.log('item', itmesNumber)
     return (
         <div className='h-screen bg-amazoncone-background'>
-            <div  className='min-w-[1000px] mx-w-[1500px] m-auto pt-8'> 
-<div className='grid grid-cols-8 bg-white'>
-    {/* Products */}
-    <div className='col-span-6'>
-        <div className='text-2xl xl:text-3xl m-4'> Shopping Cart</div>
+            <div className='min-w-[1000px] mx-w-[1500px] m-auto pt-8'>
+                <div className='grid grid-cols-8'>
+                    {/* Products */}
+                    <div className='col-span-6  bg-white'>
+                        <div className='text-2xl xl:text-3xl m-4'> Shopping Cart</div>
+                        {
+                            products.map(products => {
+                                return (<div key={products.id}>
+                                    <div className='grid grid-cols-12 divide-y  divide-gray-400 m-4'>
+
+                                        <div className='col-span-10 grid grid-cols-8 divide-y  divide-gray-400 '>
 
 
-    </div>
-    {/* Cart */}
-    <div className='col-span-2'></div>
-  
+                                            <div className='col-span-2'>
+                                                <Link to={`/product/${products.id}`}>
 
-</div>
+                                                    <img className='p-4 m-auto' src={products.image_small}></img>
+                                                </Link>
+
+                                            </div>
+                                            <div className='col-span-6'>
+                                                <div className='font-medium text-black mt-2'>
+                                                    <Link to={`/product/${products.id}`}>
+
+                                                        <ProductDetails product={products} ratings={false}></ProductDetails>
+                                                    </Link>
+                                                </div>
+
+                                                <div>
+                                                    <button className='text-sm xl:text-base font-semibold rounded-s text-blue-500 mt-2 mb-1' onClick={()=>{dispatch(removeFromCart(products.id))}}>Delete</button>
+                                                </div>
+                                                <div
+
+                                                    className='grid grid-cols-3 w-20 text-center'>
+
+                                                    <div className='text-xl xl:text-2xl bg-gray-400 rounded'>- </div>
+                                                    <div className='text-xl xl:text-xl bg-gray-200'>{products.quantity} </div>
+                                                    <div className='text-xl xl:text-2xl bg-gray-400 rounded'>+</div>
+
+                                                </div>
+
+
+                                            </div>
+                                        </div>
+
+                                        <div className='col-span-2'>
+                                            <div className='text-lg xl:text-xl mt-2 mr-4 font-semibold'>{GB_CURRENCY.format(products.price)}</div>
+                                        </div>
+                                    </div>
+
+                                </div>)
+                            })
+                        }
+                        <div className='text-lg xl:text-xl text-right mb-4 mr-4'>Subtotal({itmesNumber}itmes):<span className='font-semibold'>{GB_CURRENCY.format(subtotal)}</span></div>
+
+                    </div>
+                    {/* Checkout */}
+                    <div className='col-span-2 bg-white rounded h-[250px]' >
+
+                        <div className='text-xs xl:text-sm text text-green-800'>Your order qualifies for<span className='font-bold'>Free Delivery</span>.Delivery Details</div>
+
+                        <div className='text-base xl:text-base text-right mb-4 '>Subtotal({itmesNumber}itmes):<span className='font-semibold'>{GB_CURRENCY.format(subtotal)}</span></div>
+                        <button className='btn'>proceed to checkout</button>
+
+                    </div>
+
+
+
+
+                </div>
             </div>
-            
+
         </div>
     )
 }
--- src/Components/redux/cartSlice.js
+++ src/Components/redux/cartSlice.js
@@ -1,39 +1,52 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState={
+const initialState = {
 
-    products:[],
-    productsNumber:0
+    products: [],
+    productsNumber: 0
 
 }
-export const cartSlice=createSlice({
+export const cartSlice = createSlice({
 
-    name:"cart",
+    name: "cart",
     initialState,
-    reducers:{
-        addToCart:(state,action)=>{
-           // state.productsNumber=state.productsNumber+1
-           const addProductExists=state.products.find((product)=>product.id===action.payload.id)
-if(addProductExists){
-    console.logparseInt(action.payload.quantity)
-    addProductExists.quantity += parseInt(action.payload.quantity)
-    console.log('sssssss',addProductExists)
-    console.log(addProductExists.quantity)
+    reducers: {
+        addToCart: (state, action) => {
+            // state.productsNumber=state.productsNumber+1
+            const addProductExists = state.products.find((product) => product.id === action.payload.id)
+            if (addProductExists) {
+                console.logparseInt(action.payload.quantity)
+                addProductExists.quantity += parseInt(action.payload.quantity)
+                console.log('sssssss', addProductExists)
+                console.log(addProductExists.quantity)
 
-} else{
+            } else {
 
-    state.products.push({...action.payload,quantity:parseInt(action.payload.quantity)})
-}
+                state.products.push({ ...action.payload, quantity: parseInt(action.payload.quantity) })
+            }
 
-state.productsNumber=state.productsNumber+parseInt(action.payload.quantity)
+            state.productsNumber = state.productsNumber + parseInt(action.payload.quantity)
         },
-        removeFromCart:(state,action)=>{
+        removeFromCart: (state, action) => {
+            //find the product removing the array
+            const productToRemove = state.products.find((product) => product.id === action.payload)
 
+
+            //remove the quantity from product number
+
+            state.productsNumber = state.productsNumber - productToRemove.quantity
+            //find index of  product removing
+            const index = state.products.findIndex((product) => product.id === action.payload)
+
+
+
+            //remove the array 
+            state.products.splice(index, 1)
 
         }
     }
 
 
 })
-export const {addToCart,removeFromCart}=cartSlice.actions
-export default  cartSlice.reducer
+export const { addToCart, removeFromCart } = cartSlice.actions
+export default cartSlice.reducer
